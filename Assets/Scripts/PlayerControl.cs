3b9e4f2d8672918a98eca5d5041fd79be3eff804
OmniVRS <scottkimball01@gmail.com>
2023-11-06 10:17:34 -0800

    Day 1

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+
+    private Rigidbody playerRb;
+    private float horizontalInput;
+    public float moveSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        //This is an edit to test Github
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        playerRb.AddForce(Vector2.right * horizontalInput * moveSpeed);
     }
 }
