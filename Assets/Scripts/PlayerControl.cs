611d40e67c709a72695b3b45ad20a8baddcb2dfc
OmniVRS <scottkimball01@gmail.com>
2023-11-06 09:58:54 -0800

    Initial commit

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //This is an edit to test Github
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
